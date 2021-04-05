6e3b7178709d6d73d6989334a174f4115aaf8ee0
Thomas <kakhayavelashvili@hotmail.com>
2021-04-05 05:40:36 +0400

    some fixes

--- aws_user_data.sh
+++ aws_user_data.sh
@@ -4,18 +4,20 @@ yum update -y
 yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
-PubIP=$(curl http://checkip.amazonaws.com)
+yum install git -y
+PublicIP=$(curl http://checkip.amazonaws.com)
 PrivateIP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 # clone website project
 git clone https://github.com/LightStein/aws_task_web_part.git
 # remove default index
-rm var/www/html/index.html
+rm /var/www/html/index.html
 # copy project's index to the directory that will be hosted
 cp ./aws_task_web_part/index.html /var/www/html/
 # generate .js file and copy to host directory
-./env.sh > /var/www/html/env.js
+chmod +x ./aws_task_web_part/env.sh
+./aws_task_web_part/env.sh > /var/www/html/env.js
 
-apt-get install apache2
-service apache2 start
-apache2 chkconfig on
+sudo apt-get install apache2
+sudo service apache2 start
+sudo apache2 chkconfig on
