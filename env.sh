302193f5026f47e0d6b0f8ace565b810ab4ced45
Thomas <kakhayavelashvili@hotmail.com>
2021-04-07 18:23:43 +0400

    fix comma

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 PublicIP=$(curl http://checkip.amazonaws.com)
 PrivateIP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
-public="  PublicIP: $PublicIP"
+public="  PublicIP: $PublicIP,"
 private=" PrivateIP: $PrivateIP"
 echo "env = {"
 echo $public
