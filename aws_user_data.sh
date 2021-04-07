ac2632a89c0fe2bdff7c6d4f46e7d565b05aeab9
Thomas <kakhayavelashvili@hotmail.com>
2021-04-07 17:18:31 +0400

    test

--- aws_user_data.sh
+++ aws_user_data.sh
@@ -5,19 +5,27 @@ yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
 yum install git -y
-PublicIP=$(curl http://checkip.amazonaws.com)
-PrivateIP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
+# create variables
+PublicIP=$(curl http://checkip.amazonaws.com) # issue: no value assigned
+PrivateIP=$(echo curl http://169.254.169.254/latest/meta-data/local-ipv4) # issue: no value assigned
+# check why variables aren't asssigned ip addresses
+echo $PublicIP > /var/www/PublicIP.txt # ip address is written in this file
+echo $PrivateIP > /var/www/PrivateIP.txt # issue: this file is empty
+
+cd /var/www
 # clone website project
 git clone https://github.com/LightStein/aws_task_web_part.git
 # remove default index
 rm /var/www/html/index.html
-# copy project's index to the directory that will be hosted
+# copy project's index to the host directory
 cp ./aws_task_web_part/index.html /var/www/html/
+
 # generate .js file and copy to host directory
 chmod +x ./aws_task_web_part/env.sh
 ./aws_task_web_part/env.sh > /var/www/html/env.js
 
-sudo apt-get install apache2
-sudo service apache2 start
-sudo apache2 chkconfig on
+# sudo apt-get install apache2
+# sudo service apache2 start
+# sudo apache2 chkconfig on
+
