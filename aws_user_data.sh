f4e7780ec59fd19f79a09733401ffee9c1ad0f08
Anri <angiorganashvili@tbcbank.com.ge>
2021-04-07 23:17:49 +0400

    Fix issues

--- aws_user_data.sh
+++ aws_user_data.sh
@@ -4,18 +4,19 @@ yum update -y
 yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
-PubIP=$(curl http://checkip.amazonaws.com)
-PrivateIP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+yum install git -y
 
 # clone website project
 git clone --branch second_website --single-branch https://github.com/LightStein/aws_task_web_part.git
 # remove default index
-rm var/www/html/index.html
-# copy project's index to the directory that will be hosted
+rm /var/www/html/index.html
+# copy project's index to the host directory
 cp ./aws_task_web_part/index.html /var/www/html/
-# generate .js file and copy to host directory
-./env.sh > /var/www/html/env.js
 
-apt-get install apache2
-service apache2 start
-apache2 chkconfig on
+# generate .js file and copy to host directory
+chmod +x ./aws_task_web_part/env.sh
+./aws_task_web_part/env.sh > /var/www/html/env.js
+
+# sudo apt-get install apache2
+# sudo service apache2 start
+# sudo apache2 chkconfig on
