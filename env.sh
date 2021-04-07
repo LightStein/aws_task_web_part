f74cdf1cc91e8d65f04b3e5248897b1d77e7ca10
Thomas <kakhayavelashvili@hotmail.com>
2021-04-07 18:34:05 +0400

    fix braces

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 PublicIP=$(curl http://checkip.amazonaws.com)
 PrivateIP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
-public="  PublicIP: $PublicIP,"
-private=" PrivateIP: $PrivateIP"
+public="  PublicIP: \"$PublicIP\","
+private=" PrivateIP: \"$PrivateIP\""
 echo "env = {"
 echo $public
 echo $private
